dad72c8d74fe8852baa9531d770ccbda0b1e9449
Dzmitry Strozh <strozh@mail.ru>
2016-05-21 15:26:56 +0300

    Start to implementing NavigationView.

--- app/src/main/java/com/strozh/emailclient/MainActivity.java
+++ app/src/main/java/com/strozh/emailclient/MainActivity.java
@@ -2,17 +2,42 @@ package com.strozh.emailclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.woods.emailclient.R;
 
 /**
  * Created by Woods on 21.05.2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void setTheme(int resid) {
+        super.setTheme(R.style.AppDefault);
+    }
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
