dccb2b27617414df1e5acc9d0ab519cea817cfee
Dzmitry Strozh <strozh@mail.ru>
2016-05-21 19:37:36 +0300

    Add "Inbox" item to NavigationView and add button, where activated NavigationView.

--- app/src/main/java/com/strozh/emailclient/MainActivity.java
+++ app/src/main/java/com/strozh/emailclient/MainActivity.java
@@ -2,6 +2,9 @@ package com.strozh.emailclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -18,13 +21,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private static final int LAYOUT = R.layout.activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -36,8 +42,31 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
         toolbar.inflateMenu(R.menu.menu);
 
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()) {
+                    case R.id.action_item_inbox:
+                        showInboxLayout();
+                }
+                return true;
+            }
+        });
+    }
+
+    private void showInboxLayout() {
+        //TODO: перейти на лэйаут Inbox
     }
 }
