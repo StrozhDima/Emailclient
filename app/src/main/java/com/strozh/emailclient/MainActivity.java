f3fc3e733ce3695899f7af42d0fb5665eef1497d
Dzmitry Strozh <strozh@mail.ru>
2016-05-21 08:36:46 +0300

    Creat MainActivity class and layout

---
+++ app/src/main/java/com/strozh/emailclient/MainActivity.java
@@ -0,0 +1,18 @@
+package com.strozh.emailclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.woods.emailclient.R;
+
+/**
+ * Created by Woods on 21.05.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
