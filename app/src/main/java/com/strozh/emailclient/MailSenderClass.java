9c1bf89ac3fd728ac113e9bd9169ed69da130df5
Dzmitry Strozh <strozh@mail.ru>
2016-05-29 13:00:10 +0300

    Write code in NewMail fragment .

---
+++ app/src/main/java/com/strozh/emailclient/MailSenderClass.java
@@ -0,0 +1,8 @@
+package com.strozh.emailclient;
+
+
+/**
+ * Created by Woods on 29.05.2016.
+ */
+public class MailSenderClass {
+}
--- app/src/main/java/com/strozh/emailclient/fragments/FragmentNewMail.java
+++ app/src/main/java/com/strozh/emailclient/fragments/FragmentNewMail.java
@@ -4,17 +4,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.woods.emailclient.R;
 
+import java.io.FileInputStream;
+
 
 public class FragmentNewMail extends Fragment {
 
-    private Toolbar toolbar;
+    private Button sendMailButton;
+    private String filepath;
 
     public FragmentNewMail() {
         // Required empty public constructor
@@ -24,10 +27,19 @@ public class FragmentNewMail extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_new_mail, container, false);
+        final View view = inflater.inflate(R.layout.fragment_new_mail, container, false);
+
+        sendMailButton = (Button) view.findViewById(R.id.buttonSend);
+        sendMailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
+
 
 
 }
