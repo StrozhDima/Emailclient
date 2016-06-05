a5a3b99fc7bfab62a2c1c4d5c2289cdb517d0453
Dzmitry Strozh <strozh@mail.ru>
2016-06-05 08:32:35 +0300

    debug send mail...

---
+++ app/src/main/java/com/woods/emailclient/GetMail.java
@@ -0,0 +1,15 @@
+package com.woods.emailclient;
+
+import android.util.Log;
+
+import java.util.Properties;
+
+import javax.mail.*;
+
+/**
+ * Created by Woods on 01.06.2016.
+ */
+public class GetMail extends Authenticator {
+
+}
+
